91514e4cc65c9553476792ce750465d611a07fe5
Subash Mahanta <99633788+SubashXess@users.noreply.github.com>
2022-07-23 21:16:19 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const String googleApiKey = "AIzaSyCfDk4DRjpHpC4C6HsSuFaDV5XmkCuXHEY";
+const String googleApiKey = "API_KEY";
 const Color primaryColor = Color(0xFF7B61FF);
 const double defaultPadding = 16.0;
